4298ee1c4103698577770f9d8372eb0b0977318f
HenniePenny <h.kressner@hotmail.de>
2022-07-30 22:52:32 +0200

    html sidebar and css

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,40 @@
 const searchCity = document.getElementById("submitCity");
+//store user input in let city variable
+let cityName = document.getElementById("cityInput");
 const API_KEY = "1d6a84a28c8d241a890752aa40ebeae2";
 
-//store user input in let city variable
-let city;
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+//second API call with lat and lon for all the weather data I need.
+const getDataWithCoordinates = (lat, lon) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=1d6a84a28c8d241a890752aa40ebeae2`
+  )
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
 
-//fetch(queryURL);
+//getting data including lat and lon for second API call
+const callApi = (event) => {
+  event.preventDefault();
+  console.log(cityName.value);
 
-const callApi = () => {
-  console.log("hello!");
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${API_KEY}&units=metric`
+  )
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const lat = data.coord.lat;
+      const lon = data.coord.lon;
+      getDataWithCoordinates(lat, lon);
+    });
 };
 
 searchCity.addEventListener("click", callApi);
