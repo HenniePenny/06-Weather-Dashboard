06ecd406e60ed43a0f41b6205a73a9834c6b631b
HenniePenny <h.kressner@hotmail.de>
2022-07-31 12:53:29 +0200

    render weather & 5-day-forecast

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const cityName = document.getElementById("cityInput");
 const API_KEY = "1d6a84a28c8d241a890752aa40ebeae2";
 
 //second API call with lat and lon for all the weather data I need.
-const getDataWithCoordinates = (lat, lon) => {
+const getDataWithCoordinates = (lat, lon, cityName) => {
   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=1d6a84a28c8d241a890752aa40ebeae2&units=metric`;
   fetch(url)
     .then((res) => {
@@ -15,32 +15,33 @@ const getDataWithCoordinates = (lat, lon) => {
       console.log(data);
       const renderWeatherData = {
         date: data.current.dt,
-        icon: data.current.weather.icon,
+        icon: data.current.weather[0].icon,
         temp: data.current.temp,
         wind: data.current.wind_speed,
         humidity: data.current.humidity,
         uvindex: data.current.uvi,
       };
-      //   renderWeather(renderWeatherData);
+      renderWeather(renderWeatherData, cityName);
+      get5DayForecast(data.daily);
     });
 };
 
-const get5DayForecast = (lat, lon) => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=1d6a84a28c8d241a890752aa40ebeae2&units=metric`;
-  fetch(url)
-    .then((res) => {
-      console.log(res);
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const renderForecastData = {
-        date: data.daily[1].dt,
-        temp: data.daily[1].temp.day,
-        wind: data.daily[1].wind_speed,
-        humidity: data.daily[1].humidity,
-      };
-    });
+const renderWeather = (renderWeatherData, cityName) => {
+  var dateString = moment.unix(renderWeatherData.date).format("MM/DD/YYYY");
+  $("#displayCityDateIcon").text(`${cityName} ${dateString}`);
+  let icon = $("<img>").attr(
+    "src",
+    `http://openweathermap.org/img/wn/${renderWeatherData.icon}.png`
+  );
+  $("#displayCityDateIcon").append(icon);
+  $("#wind").text(`wind: ${renderWeatherData.wind}`);
+};
+
+const get5DayForecast = (dailyData) => {
+  for (let index = 0; index < 5; index++) {
+    const element = dailyData[index];
+    $(`#day${index + 1} .humidity`).text(`humidity: ${element.humidity}`);
+  }
 };
 
 //getting data including lat and lon for second API call
@@ -59,7 +60,7 @@ const callApi = (event) => {
       console.log(data);
       const lat = data.coord.lat;
       const lon = data.coord.lon;
-      getDataWithCoordinates(lat, lon);
+      getDataWithCoordinates(lat, lon, data.name);
     });
 };
 
