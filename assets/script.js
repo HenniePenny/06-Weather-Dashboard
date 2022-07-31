3c2fe465a163debbc3d9effd031637478c7a7b91
HenniePenny <h.kressner@hotmail.de>
2022-07-31 11:59:18 +0200

    empty divs forecast & JS data from call

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,41 @@ const API_KEY = "1d6a84a28c8d241a890752aa40ebeae2";
 
 //second API call with lat and lon for all the weather data I need.
 const getDataWithCoordinates = (lat, lon) => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=1d6a84a28c8d241a890752aa40ebeae2`
-  )
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=1d6a84a28c8d241a890752aa40ebeae2&units=metric`;
+  fetch(url)
     .then((res) => {
       console.log(res);
       return res.json();
     })
     .then((data) => {
       console.log(data);
+      const renderWeatherData = {
+        date: data.current.dt,
+        icon: data.current.weather.icon,
+        temp: data.current.temp,
+        wind: data.current.wind_speed,
+        humidity: data.current.humidity,
+        uvindex: data.current.uvi,
+      };
+      //   renderWeather(renderWeatherData);
+    });
+};
+
+const get5DayForecast = (lat, lon) => {
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=1d6a84a28c8d241a890752aa40ebeae2&units=metric`;
+  fetch(url)
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const renderForecastData = {
+        date: data.daily[1].dt,
+        temp: data.daily[1].temp.day,
+        wind: data.daily[1].wind_speed,
+        humidity: data.daily[1].humidity,
+      };
     });
 };
 
